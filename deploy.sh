4293d7aa56ed59ac324c1ea11da0aaa73a30e822
mgarbowski <mikolaj.garbowski@gmail.com>
2023-10-19 19:47:38 +0200

    deployment script and job

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+start=$(date +"%s")
+
+ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
+docker pull mgarbowski/pap-projekt-backend:latest
+
+CONTAINER_NAME=pap-backend
+if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
+    if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+        echo "Container is running -> stopping it..."
+        docker stop $CONTAINER_NAME;
+    fi
+fi
+
+docker run -d --rm -p 8080:8080 --name $CONTAINER_NAME mgarbowski/pap-projekt-backend:latest
+
+exit
+ENDSSH
+
+if [ $? -eq 0 ]; then
+  exit 0
+else
+  exit 1
+fi
+
+end=$(date +"%s")
+
+diff=$(($end - $start))
+
+echo "Deployed in : ${diff}s"

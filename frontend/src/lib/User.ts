fe2015fe026d65bc11625b984d2705bc67ab19c8
mgarbowski <mikolaj.garbowski@gmail.com>
2023-11-02 13:10:15 +0100

    refactor user login logic

--- frontend/src/lib/User.ts
+++ frontend/src/lib/User.ts
@@ -1,28 +1,24 @@
-export class User {
-    username: string;
-    password: string;
-    token?: string;
+export interface LoginRequest {
+    username: string,
+    password: string,
+}
 
+export interface User extends LoginRequest {
+    token: string,
+}
 
-    constructor(username: string, password: string) {
-        this.username = username;
-        this.password = password;
-    }
-
-    async requestToken() {
-        try {
-            const response = await fetch(`${process.env.REACT_APP_API_ROOT}auth/login`, {
-                method: "POST",
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(this),
-            });
-            const json = await response.json();
-            this.token = json.accessToken;
-        } catch (e) {
-            console.error(e)
-        }
-    }
+export async function attemptLogin(loginRequest: LoginRequest): Promise<User> {
+    const response = await fetch(`${process.env.REACT_APP_API_ROOT}auth/login`, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(loginRequest),
+    });
+    const json = await response.json();
+    return {
+        ...loginRequest,
+        token: json.accessToken
+    };
 }
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,39 +1,33 @@
 import {useEffect, useState} from "react";
-import { useJwt } from "react-jwt";
+import {useJwt} from "react-jwt";
+import {attemptLogin, LoginRequest, User} from "../lib/User";
 
 export function Login() {
-    const user = {
-        username: "rdeckard",
-        password: "password"
-    }
-    const [token, setToken] = useState<string>("");
+    const loginRequest: LoginRequest = {username: "rdeckard", password: "password"}
+    const [user, setUser] = useState<User | null>(null);
     const [isLoaded, setIsLoaded] = useState(false);
 
-    const {decodedToken, isExpired} = useJwt(token);
-
     useEffect(() => {
-        fetch("http://localhost:8080/api/auth/login", {
-            method: "POST",
-            body: JSON.stringify(user),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-        })
-            .then(response => response.json())
-            .then(json => {
-                console.log(json)
-                setToken(json.accessToken)
+        attemptLogin(loginRequest)
+            .then(u => {
+                setUser(u)
                 setIsLoaded(true)
             })
-            .catch(e => console.error(e))
-    })
-
+    }, [])
 
     return <>
         <h1>Login</h1>
-        <p>{isLoaded ? token : "Loading..."}</p>
-        {isLoaded ? <p>{JSON.stringify(decodedToken)}</p> : null}
-        <p>{isExpired ? "Expired" : "Valid"}</p>
+        {isLoaded ? <Welcome user={user!}/> : <p>Loading...</p>}
     </>;
+}
+
+
+export function Welcome({user}: { user: User }) {
+    console.log(user);
+    const {decodedToken, isExpired} = useJwt(user.token!);
+    return <h1>
+        <p>Welcome {user.username}</p>
+        <p>Your token is {isExpired ? "expired" : "valid"}</p>
+        <p>{JSON.stringify(decodedToken)}</p>
+    </h1>
 }

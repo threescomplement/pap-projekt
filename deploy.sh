69af6792b4f9676ee38573805207bb63167c9db0
mgarbowski <mikolaj.garbowski@gmail.com>
2023-11-12 23:40:51 +0100

    change deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ start=$(date +"%s")
 
 scp -i key.txt -P ${SERVER_PORT} -o StrictHostKeyChecking=no ./docker-compose.yml ${SERVER_USER}@${SERVER_HOST}:/srv/pap/
 
-ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
+ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -t -t -o StrictHostKeyChecking=no << ENDSSH
 
 cd /srv/pap
 cat<<EOF > /srv/pap/docker-compose.deployment.yml

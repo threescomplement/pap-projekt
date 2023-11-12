36059f1bdc1d689a0e66d64b5dcb68088db9d9e9
mgarbowski <mikolaj.garbowski@gmail.com>
2023-11-12 23:37:36 +0100

    change deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 start=$(date +"%s")
-#echo "Copying docker-compose.yml"
-#scp -i key.txt -P ${SERVER_PORT} -o StrictHostKeyChecking=no ./docker-compose.yml ${SERVER_USER}@${SERVER_HOST}:/srv/pap/
+
+scp -i key.txt -P ${SERVER_PORT} -o StrictHostKeyChecking=no ./docker-compose.yml ${SERVER_USER}@${SERVER_HOST}:/srv/pap/
 
 ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
 

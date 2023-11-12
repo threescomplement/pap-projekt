ead369895ea34f4984bd5270d64bdea49e4e8960
mgarbowski <mikolaj.garbowski@gmail.com>
2023-11-12 21:35:42 +0100

    handle environment changes in frontend app

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,7 @@
 export function Home() {
-    return <h1>Home {process.env.REACT_APP_API_ROOT}</h1>
+    return <>
+        <h1>Home</h1>
+        <p>API root {process.env.REACT_APP_API_ROOT}</p>
+        <p>Environment {process.env.NODE_ENV}</p>
+    </>;
 }

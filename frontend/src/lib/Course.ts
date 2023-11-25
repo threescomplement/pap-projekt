b391f90309a1ca7640ef418f8c85f66d75bda320
maksym <01178511@pw.edu.pl>
2023-11-25 14:29:26 +0100

    update ICourse interface to represent data returned by the api, implemented async function requesting the data of a course

--- frontend/src/lib/Course.ts
+++ frontend/src/lib/Course.ts
@@ -1,4 +1,20 @@
 export interface ICourse {
     name: string,
-    _links: any
+    language: string,
+    type: string | null, // TODO: can type be null?
+    level: string | null, // TODO: can level be null?
+    module: string | null, //TODO: make api return teacher id
+    links: any
+}
+
+
+export async function attemptCourseDataRequest(courseId: string) {
+    /**
+     * Request the data of a course
+     * @param courseId - id of the course
+     * */
+    // TODO: courseId does not exist in the database
+    return await fetch(`${process.env.REACT_APP_API_ROOT}courses/${Number(courseId)}`)
+        .then(response => response.json())
+        .catch(e => console.error(e));
 }

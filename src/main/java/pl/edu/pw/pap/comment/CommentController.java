dce06e6684683ed3478cd05f50647a0052e39ae2
mgarbowski <mikolaj.garbowski@gmail.com>
2023-11-27 22:32:33 +0100

    links and authorization

--- src/main/java/pl/edu/pw/pap/comment/CommentController.java
+++ src/main/java/pl/edu/pw/pap/comment/CommentController.java
@@ -1,11 +1,17 @@
 package pl.edu.pw.pap.comment;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.pw.pap.security.UserPrincipal;
 
-import java.util.List;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,8 +19,17 @@ public class CommentController {
     private final CommentService commentService;
 
     @GetMapping("/api/courses/{courseId}/reviews/{reviewerId}/comments")
-    public List<Comment> getCommentsForReview(@PathVariable Long courseId, @PathVariable Long reviewerId) {
+    public CollectionModel<Comment> getCommentsForReview(@PathVariable Long courseId, @PathVariable Long reviewerId) {
         var comments = commentService.getCommentsForReview(courseId, reviewerId);
-        return comments;
+        return CollectionModel.of(
+                comments,
+                linkTo(methodOn(CommentController.class).getCommentsForReview(courseId, reviewerId)).withSelfRel()
+        );
+    }
+
+    @DeleteMapping("/api/comments/{commentId}")
+    public ResponseEntity<Comment> deleteComment(@PathVariable Long commentId, @AuthenticationPrincipal UserPrincipal principal) {
+        commentService.deleteComment(commentId, principal);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/pl/edu/pw/pap/comment/CommentService.java
+++ src/main/java/pl/edu/pw/pap/comment/CommentService.java
@@ -3,6 +3,8 @@ package pl.edu.pw.pap.comment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.edu.pw.pap.review.ReviewKey;
+import pl.edu.pw.pap.security.UserPrincipal;
+import pl.edu.pw.pap.user.UserRepository;
 
 import java.util.List;
 
@@ -10,9 +12,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
 
     public List<Comment> getCommentsForReview(Long courseId, Long reviewerId) {
         return commentRepository.findByReview_Id(new ReviewKey(reviewerId, courseId));
     }
 
+    public void deleteComment(Long commentId, UserPrincipal principal) {
+        var comment = commentRepository.findById(commentId).orElseThrow();
+        var user = userRepository.findByUsername(principal.getUsername()).orElseThrow();
+        if (!comment.getUser().getId().equals(user.getId())) {
+            throw new UnauthorizedException("User can only delete his own comments");
+        }
+
+        commentRepository.delete(comment);
+    }
 }

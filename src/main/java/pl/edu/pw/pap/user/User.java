d1b550da15884cfefb5abd2354e43bcb908e4ba3
Michal Luszczek <michluszcz@gmail.com>
2023-11-29 22:20:22 +0100

    Added Unique constraint to username

--- src/main/java/pl/edu/pw/pap/user/User.java
+++ src/main/java/pl/edu/pw/pap/user/User.java
@@ -18,8 +18,11 @@ public class User {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(unique = true)
     private String username;
     private String email;
+
     @JsonIgnore
     private String password;
     private String role;

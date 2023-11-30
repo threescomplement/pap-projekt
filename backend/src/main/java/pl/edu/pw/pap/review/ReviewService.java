3d35403b21c3215a34bc7c0962ce13e9416cf100
Michal Luszczek <michluszcz@gmail.com>
2023-12-01 00:27:55 +0100

    Created ReviewService, getReviewById and getCourseReviews

--- backend/src/main/java/pl/edu/pw/pap/review/ReviewService.java
+++ backend/src/main/java/pl/edu/pw/pap/review/ReviewService.java
@@ -2,8 +2,36 @@ package pl.edu.pw.pap.review;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.edu.pw.pap.course.Course;
+import pl.edu.pw.pap.course.CourseRepository;
+import pl.edu.pw.pap.course.courseNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
+    private final ReviewRepository reviewRepository;
+    private final CourseRepository courseRepository;
+
+
+    public Optional<Review> getReviewById(Long userId, Long courseId ){
+        ReviewKey reviewKey = new ReviewKey(userId, courseId);
+        return reviewRepository.findById(reviewKey);
+    }
+
+    public List<Review> getCourseReviews(Long courseId) {
+        Optional<Course> getCourse = courseRepository.findById(courseId);
+        if (getCourse.isEmpty()){
+            throw new courseNotFoundException("No course with id " + courseId);
+        }
+        Course course = getCourse.get();
+        return new ArrayList<Review>(course.getReviews());
+
+
+    }
 }

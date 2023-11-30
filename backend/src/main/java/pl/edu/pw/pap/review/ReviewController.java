b17d3e253fa84c640204842108b33da1fdf9693c
Michal Luszczek <michluszcz@gmail.com>
2023-11-30 13:43:08 +0100

    Created empty ReviewController and ReviewService

---
+++ backend/src/main/java/pl/edu/pw/pap/review/ReviewController.java
@@ -0,0 +1,10 @@
+package pl.edu.pw.pap.review;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReviewController {
+}
---
+++ backend/src/main/java/pl/edu/pw/pap/review/ReviewService.java
@@ -0,0 +1,9 @@
+package pl.edu.pw.pap.review;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+}

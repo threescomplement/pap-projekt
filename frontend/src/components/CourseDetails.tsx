edf04886b0671e3a91ff817aa529531931b22fd6
maksym <01178511@pw.edu.pl>
2023-12-04 19:08:00 +0100

    refactoring of single course website 2

--- frontend/src/components/CourseDetails.tsx
+++ frontend/src/components/CourseDetails.tsx
@@ -2,7 +2,7 @@ import {Course} from "../lib/Course";
 import {Teacher, TeacherService} from "../lib/Teacher";
 import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
-import {COURSE_TEACHER_PLACEHOLDER} from "../lib/utils";
+import {COURSE_TEACHER_PLACEHOLDER, NUM_REVIEWS_PLACEHOLDER} from "../lib/utils";
 
 
 export default function CourseDetails(course: Course) {
@@ -18,12 +18,27 @@ export default function CourseDetails(course: Course) {
 
     }, []);
 
-    const teacherContent = (teacher != null && teacherLoaded) ? <Link to={"/teachers/" + course.teacherId}>
-        {teacher.name}
-    </Link> : COURSE_TEACHER_PLACEHOLDER;
+    const teacherContent = (teacher != null && teacherLoaded)
+        ? <Link to={"/teachers/" + course.teacherId}> {teacher.name} </Link>
+        : COURSE_TEACHER_PLACEHOLDER;
+
+    const moduleContent = course.module !== null
+        ? <p>Moduł: {course.module}</p>
+        : <p>Ten kurs nie jest przypisany do żadnego modułu</p>
+
+    const levelContent = <p>Poziom: {course.level}</p>
+    const typeContent = <p>Typ kursu: {course.type}</p>
 
     return <>
         <h1>{course.name}</h1>
         <h2>Lektor: {teacherContent}</h2>
+        <h3>Informacje o kursie:</h3>
+        {moduleContent}
+        {levelContent}
+        {typeContent}
+        <h3>
+            Kurs ma {NUM_REVIEWS_PLACEHOLDER} opinii.<br></br>
+            <Link to={"/courses/" + course.id}>Czytaj opinie (placeholder)</Link>
+        </h3>
     </>
 }

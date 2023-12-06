d7bbbf25def433aa57bf3174057a0b270a4016e3
maksym <01178511@pw.edu.pl>
2023-12-07 00:29:40 +0100

    add link to comments below CommentReviewCard

--- frontend/src/components/CommentReviewCard.tsx
+++ frontend/src/components/CommentReviewCard.tsx
@@ -1,4 +1,6 @@
 import {ReviewComment} from "../lib/ReviewComment";
+import React from "react";
+import {Link} from "react-router-dom";
 
 
 interface CommentReviewCardProps {
@@ -10,12 +12,12 @@ export function CommentReviewCard({data}: CommentReviewCardProps) {
         <div>{data.authorUsername}</div>
         <div>{'overallRating' in data && "Ocena: " + data.overallRating}</div>
         <div>{data.opinion}</div>
-        <div>{'overallRating' in data._links && "This is a review"}</div>
-    </>
-}
+        <div>
+            {'overallRating' in data &&
+                <Link to={"comments/" + data.authorUsername}> Czytaj więcej </Link>}
+                </div>
+                </>
+            }
 
 
-
-
-
-//todo: edit, delete, etc based on the user logged in
+            //todo: edit, delete, etc based on the user logged in
--- frontend/src/pages/review/SingleReview.tsx
+++ frontend/src/pages/review/SingleReview.tsx
@@ -1,6 +1,6 @@
 import {useParams} from "react-router-dom";
 
 export function SingleReview() {
-    const {reviewId} = useParams();
-    return <h1>dupa</h1>
+    const {authorUsername} = useParams();
+    return <h1>Placeholder</h1>
 }

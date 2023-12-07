da708ba2298337206c2e73f98ff845b3c7237557
Michal Luszczek <michluszcz@gmail.com>
2023-12-08 00:58:02 +0100

    Created getTeacherCourses and convertToDto in CourseService (the second one poorly)

--- backend/src/main/java/pl/edu/pw/pap/course/CourseService.java
+++ backend/src/main/java/pl/edu/pw/pap/course/CourseService.java
@@ -2,6 +2,8 @@ package pl.edu.pw.pap.course;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.edu.pw.pap.teacher.TeacherNotFoundException;
+import pl.edu.pw.pap.teacher.TeacherRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,9 +14,33 @@ import static pl.edu.pw.pap.common.Constants.ALL;
 @RequiredArgsConstructor
 public class CourseService {
     private final CourseRepository courseRepository;
+    private final TeacherRepository teacherRepository;
+
+
+    public CourseDTO convertToDto(Course course){
+        // An additional call to the database is bad but otherwise I'd have to make an averageRating function which
+        // Would have already been done earlier I imagine
+        // TODO: Consider getting the average rating of a course by just counting the ratings from course.getRatings()
+
+        return courseRepository
+                .findByIdWithRating(course.getId())
+                .orElseThrow( () -> new CourseNotFoundException("Conversion to CourseDTO failed"));
+    }
 
     public Optional<CourseDTO> getById(Long courseId) {
         return courseRepository.findByIdWithRating(courseId);
+    }
+
+    public List<CourseDTO> getTeacherCourses(Long teacherId){
+        var teacher = teacherRepository
+                    .findById(teacherId)
+                    .orElseThrow( () -> new TeacherNotFoundException("No teacher with given id: " + teacherId));
+        return teacher
+                .getCourses()
+                .stream()
+                .map(this::convertToDto)
+                .toList();
+
     }
 
     public List<CourseDTO> getAllMatchingFilters(String name, String language, String module, String type, String level, String teacherName) {

a76e4c5cb1b7c392f3b7f51fad6a96a8ea8a8f97
maksym <01178511@pw.edu.pl>
2023-12-08 16:20:33 +0100

    Add CSS module to Home page
    .

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,12 @@
+
+import styles from './Home.module.css';
+
 export function Home() {
-    return <>
-        <h1>Home</h1>
-        <p>API root {process.env.REACT_APP_API_ROOT}</p>
-        <p>Environment {process.env.NODE_ENV}</p>
-    </>;
+    return (
+        <div className={styles.homeContainer}>
+            <h1>Home</h1>
+            <p>API root {process.env.REACT_APP_API_ROOT}</p>
+            <p>Environment {process.env.NODE_ENV}</p>
+        </div>
+    );
 }

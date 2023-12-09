2365de88c07c1087147d800996324f2c6c2a3376
Sawertynn <sawertynn@gmx.com>
2023-12-09 02:10:36 +0100

    added all basic comment controller tests, 2 not passing, waiting on decision about behaviour

--- backend/src/test/java/pl/edu/pw/pap/comment/CommentIntegrationTest.java
+++ backend/src/test/java/pl/edu/pw/pap/comment/CommentIntegrationTest.java
@@ -18,6 +18,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import pl.edu.pw.pap.PapApplication;
 import pl.edu.pw.pap.course.Course;
 import pl.edu.pw.pap.course.CourseRepository;
+import pl.edu.pw.pap.review.AddReviewRequest;
 import pl.edu.pw.pap.review.Review;
 import pl.edu.pw.pap.review.ReviewRepository;
 import pl.edu.pw.pap.security.AuthService;
@@ -26,7 +27,10 @@ import pl.edu.pw.pap.teacher.TeacherRepository;
 import pl.edu.pw.pap.user.User;
 import pl.edu.pw.pap.user.UserRepository;
 
+import java.util.*;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static pl.edu.pw.pap.utils.UrlBuilder.buildUrl;
@@ -79,7 +83,7 @@ public class CommentIntegrationTest {
     private static final Comment COMMENT_11_3 = new Comment("rel", REVIEW_11, USER_3);
     private static final Comment COMMENT_13_2 = new Comment("czyli co?", REVIEW_13, USER_2);
     private static final Comment COMMENT_21_1 = new Comment("przesada", REVIEW_21, USER_1);
-    private static final Comment COMMENT_21_2 = new Comment("trudne mówię wam", REVIEW_21, USER_2);
+    private static final Comment COMMENT_21_2 = new Comment("trudne serio", REVIEW_21, USER_2);
     private static final Comment COMMENT_21_3 = new Comment("oj tak", REVIEW_21, USER_3);
     // private static final Comment COMMENT_24_3 = new Comment("zgadzam się", REVIEW_24, USER_3);
 
@@ -87,7 +91,7 @@ public class CommentIntegrationTest {
         teacherRepository.saveAll(List.of(TEACHER_1, TEACHER_2));
         courseRepository.saveAll(List.of(COURSE_1, COURSE_2, COURSE_3, COURSE_4));
         reviewRepository.saveAll(List.of(REVIEW_11, REVIEW_21, REVIEW_24, REVIEW_13));
-        commentRepository.saveAll(List.of(COMMENT_21_1, COMMENT_13_2, COMMENT_11_3, COMMENT_21_2, COMMENT_21_3));
+        commentRepository.saveAll(List.of(COMMENT_11_3, COMMENT_21_1, COMMENT_13_2, COMMENT_21_2, COMMENT_21_3));
     }
 
 
@@ -117,91 +121,213 @@ public class CommentIntegrationTest {
                 HttpMethod.GET, new HttpEntity<>(headers), String.class);
         assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
         var json = JsonPath.parse(response.getBody());
-        assertEquals("[]", json.read("$._embedded.comments"));
+        assertEquals("[]", json.read("$._embedded.comments").toString());
 
     }
     @Test
     public void getCommentsForReviewSingleExists() {
         addDummyData();
-        var response = restTemplate.exchange(buildUrl("/api/courses/1/reviews/user_2/comments", port),
-                HttpMethod.GET, new HttpEntity<>(headers), String.class);
-        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
-        var json = JsonPath.parse(response.getBody());
-
-    }
-    @Test
-    public void getCommentsForReviewMultipleExist() {
-        addDummyData();
-
         var response = restTemplate.exchange(buildUrl("/api/courses/1/reviews/user_1/comments", port),
                 HttpMethod.GET, new HttpEntity<>(headers), String.class);
         assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
         var json = JsonPath.parse(response.getBody());
-        List<String> comments = json.read("$._embedded.comments");
+        assertEquals(1, (int) json.read("$._embedded.comments[0].id"));
+        assertEquals("rel", json.read("$._embedded.comments[0].text"));
+        assertEquals("user_3", json.read("$._embedded.comments[0].authorUsername"));
+        assertTrue(json.read("$._embedded.comments[0].created").toString().endsWith("+00:00"));
 
+        // check links
+        assertTrue(json.read("$._links.self.href").toString().endsWith("/api/courses/1/reviews/user_1/comments"));
+        assertTrue(json.read("$._embedded.comments[0]._links.self.href").toString().endsWith("/api/comments/1"));
+        assertTrue(json.read("$._embedded.comments[0]._links.review.href").toString().endsWith("/api/courses/1/reviews/user_3"));
+        assertTrue(json.read("$._embedded.comments[0]._links.user.href").toString().endsWith("/api/users/user_3"));
+    }
+    @Test
+    public void getCommentsForReviewMultipleExist() {
+        addDummyData();
+        var response = restTemplate.exchange(buildUrl("/api/courses/1/reviews/user_2/comments", port),
+                HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        var json = JsonPath.parse(response.getBody());
+        List<String> comments = json.read("$._embedded.comments");
+        assertEquals(3, comments.size());
+        String comString = comments.toString();
+        assertTrue(comString.contains("\"text\":\"przesada\""));
+        assertTrue(comString.contains("\"text\":\"oj tak\""));
+        assertTrue(comString.contains("\"text\":\"trudne serio\""));
     }
 
 
     // get "/api/comments/{commentId}"
     @Test
     public void getCommentByIdNotExists() {
-        fail();
+        var response = restTemplate.exchange(buildUrl("/api/comments/1", port),
+                HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(404), response.getStatusCode());
     }
     @Test
     public void getCommentByIdExists() {
-        fail();
+        addDummyData();
+        var response = restTemplate.exchange(buildUrl("/api/comments/1", port),
+                HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        var json = JsonPath.parse(response.getBody());
+        LinkedHashMap<String, String> com = json.read("$");
+        assertEquals(1, com.get("id"));
+        assertEquals("rel", com.get("text"));
+        assertEquals("user_3", com.get("authorUsername"));
+        assertTrue(com.containsKey("created"));
     }
 
     // get "/api/users/{username}/comments"
     @Test
     public void getCommentsByUsernameNotExists() {
+        var response = restTemplate.exchange(buildUrl("/api/users/nope/comments", port),
+                HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        // TODO: what is the behaviour we want? fail or succeed empty?
+        assertEquals(HttpStatusCode.valueOf(404), response.getStatusCode());
         fail();
     }
 
     @Test
     public void getCommentsByUsernameSingleExists() {
-        fail();
+        // COMMENT_21_1 no. 3
+        addDummyData();
+        var response = restTemplate.exchange(buildUrl("/api/users/user_1/comments", port),
+                HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        var json = JsonPath.parse(response.getBody());
+        LinkedHashMap<String, String> com = json.read("$._embedded.comments[0]");
+        assertEquals(2, com.get("id"));
+        assertEquals("przesada", com.get("text"));
+        assertEquals("user_1", com.get("authorUsername"));
+        assertTrue(com.containsKey("created"));
+        String userLink = json.read("$._links.user.href");
+        assertTrue(userLink.endsWith("api/users/user_1"));
     }
     @Test
     public void getCommentsByUsernameMultipleExist() {
-        fail();
+        addDummyData();
+        var response = restTemplate.exchange(buildUrl("/api/users/user_3/comments", port),
+                HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        var json = JsonPath.parse(response.getBody());
+        List<LinkedHashMap<String, String>> comments = json.read("$._embedded.comments");
+        assertEquals(2, comments.size());
+
+        int pos =  0;
+        assertEquals(1, comments.get(pos).get("id"));
+        assertEquals("user_3", comments.get(pos).get("authorUsername"));
+        assertEquals("rel", comments.get(pos).get("text"));
+        assertTrue(comments.get(pos).containsKey("created"));
+
+        pos =  1;
+        assertEquals(5, comments.get(pos).get("id"));
+        assertEquals("user_3", comments.get(pos).get("authorUsername"));
+        assertEquals("oj tak", comments.get(pos).get("text"));
+        assertTrue(comments.get(pos).containsKey("created"));
     }
 
     // post "/api/courses/{courseId}/reviews/{reviewerUsername}/comments"
 
     @Test
     public void addCommentCourseNotExists() {
-        fail();
+        addDummyData();
+        var request = new AddCommentRequest("ok");
+        var response = restTemplate.exchange(buildUrl("/api/courses/420/reviews/user_1/comments", port),
+                HttpMethod.POST, new HttpEntity<>(request, headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(404), response.getStatusCode());
     }
     @Test
     public void addCommentReviewNotExists() {
-        fail();
+        addDummyData();
+        var request = new AddCommentRequest("ok");
+        var response = restTemplate.exchange(buildUrl("/api/courses/4/reviews/user_1/comments", port),
+                HttpMethod.POST, new HttpEntity<>(request, headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(404), response.getStatusCode());
+
     }
     @Test
     public void addFirstCommentForReview() {
-        fail();
+        addDummyData();
+        String endpoint = "/api/courses/4/reviews/user_2/comments";
+        var request = new AddCommentRequest("ok");
+        var response = restTemplate.exchange(buildUrl(endpoint, port),
+                HttpMethod.POST, new HttpEntity<>(request, headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+
+        response = restTemplate.exchange(buildUrl(endpoint, port),
+                HttpMethod.GET, new HttpEntity<>(request, headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        var json = JsonPath.parse(response.getBody());
+        LinkedHashMap<String, String> com = json.read("$._embedded.comments[0]");
+        assertEquals("ok", com.get("text"));
+        assertEquals("user_1", com.get("authorUsername"));
+        assertTrue(com.containsKey("id"));
+        assertTrue(com.containsKey("created"));
     }
     @Test
     public void addSecondCommentForReview() {
-        fail();
+        addDummyData();
+        String endpoint = "/api/courses/3/reviews/user_1/comments";
+        var request = new AddCommentRequest("ok");
+        var response = restTemplate.exchange(buildUrl(endpoint, port),
+                HttpMethod.POST, new HttpEntity<>(request, headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+
+        response = restTemplate.exchange(buildUrl(endpoint, port),
+                HttpMethod.GET, new HttpEntity<>(request, headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        var json = JsonPath.parse(response.getBody());
+        LinkedHashMap<String, String> com = json.read("$._embedded.comments[1]");
+        assertEquals("ok", com.get("text"));
+        assertEquals("user_1", com.get("authorUsername"));
+        assertTrue(com.containsKey("id"));
+        assertTrue(com.containsKey("created"));
     }
 
     // delete "/api/comments/{commentId}"
     @Test
     public void deleteCommentNotExists() {
+        addDummyData();
+        var response = restTemplate.exchange(buildUrl("/api/comments/420", port),
+                HttpMethod.DELETE, new HttpEntity<>(headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(204), response.getStatusCode());
+    }
+    @Test
+    public void deleteCommentDifferentUser() {;
+        addDummyData();
+        String endpoint = "/api/comments/1"; // by user_3
+        var response = restTemplate.exchange(buildUrl(endpoint, port),
+                HttpMethod.DELETE, new HttpEntity<>(headers), String.class);
+        // TODO: what do we expect?
+        assertEquals(HttpStatusCode.valueOf(403), response.getStatusCode());
         fail();
     }
     @Test
-    public void deleteCommentDifferentUser() {
-        fail();
-    }
-    @Test
-    public void deleteCommentExists() {
-        fail();
+    public void deleteCommentExistsGoodUser() {
+        addDummyData();
+        String endpoint = "/api/comments/2";
+        var response = restTemplate.exchange(buildUrl(endpoint, port),
+                HttpMethod.DELETE, new HttpEntity<>(headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(204), response.getStatusCode());
+
+        response = restTemplate.exchange(buildUrl(endpoint, port),
+                HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(404), response.getStatusCode());
     }
     @Test
     public void deleteCommentByAdmin() {
-        fail();
+        addDummyData();
+        adminLogin();
+        String endpoint = "/api/comments/1";
+        var response = restTemplate.exchange(buildUrl(endpoint, port),
+                HttpMethod.DELETE, new HttpEntity<>(headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(204), response.getStatusCode());
+
+        response = restTemplate.exchange(buildUrl(endpoint, port),
+                HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        assertEquals(HttpStatusCode.valueOf(404), response.getStatusCode());
     }
 }
 

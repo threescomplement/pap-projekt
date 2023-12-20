4a89220d4706ff65de45fd7d0c118f56ecabe436
maksym <01178511@pw.edu.pl>
2023-12-20 15:08:02 +0100

    basic ahh implementation of removing comments, not tested yet but have to commit and get back to it later

--- frontend/src/components/ReviewCards.tsx
+++ frontend/src/components/ReviewCards.tsx
@@ -1,14 +1,24 @@
-import {Review} from "../lib/Review";
-import {Link} from "react-router-dom";
+import {Review, ReviewService} from "../lib/Review";
+import {Link, useParams} from "react-router-dom";
+import {EditBar} from "./EditBar";
 import React from "react";
+import useUser from "../hooks/useUser";
+import {User} from "../lib/User";
 
 interface ReviewCardProps {
     review: Review;
 }
 
 export function ReviewCardWithLink({review}: ReviewCardProps) {
+    const {courseId} = useParams()
+    const user: User = useUser().user!;
+    const isAdmin: boolean = user.roles[0] === "ROLE_ADMIN";
+    const ownComment: boolean = review.authorUsername === user.username;
+    const modificationContent = (ownComment || isAdmin) ?
+        <EditBar handleDelete={createDeleteHandler(courseId!, review.authorUsername)}/> : null; // todo
+
     return <>
-        <div>{review.authorUsername}</div>
+        <div>{review.authorUsername} {modificationContent}</div>
         <div>{"Ocena: " + review.overallRating}</div>
         <div>{review.opinion}</div>
         <div>
@@ -25,3 +35,16 @@ export function ReviewCardWithoutLink({review}: ReviewCardProps) {
     </>
 }
 
+
+function createDeleteHandler(courseId: string, username: string): React.MouseEventHandler {
+    return async event => {
+        event.preventDefault();
+        try {
+            await ReviewService.deleteReview(courseId, username);
+            alert('Review deleted successfully!');
+        } catch (error) {
+            console.error(`Failed to delete review: `, error);
+            alert('Failed to delete review! Please try again...')
+        }
+    };
+}

f4332aec9c288e7d28a0bad15db92dd9f77e0b90
mgarbowski <mikolaj.garbowski@gmail.com>
2023-12-27 20:12:00 +0100

    extract dummy data generation to a separate class

--- backend/src/main/java/pl/edu/pw/pap/PapApplication.java
+++ backend/src/main/java/pl/edu/pw/pap/PapApplication.java
@@ -19,6 +19,7 @@ import pl.edu.pw.pap.teacher.Teacher;
 import pl.edu.pw.pap.teacher.TeacherRepository;
 import pl.edu.pw.pap.user.User;
 import pl.edu.pw.pap.user.UserRepository;
+import pl.edu.pw.pap.utils.DummyDataGenerator;
 
 import java.util.List;
 
@@ -31,7 +32,7 @@ public class PapApplication {
         SpringApplication.run(PapApplication.class, args);
     }
 
-//    @Bean
+    @Bean
     @Profile({"dev", "dev-postgres"})
     public CommandLineRunner addDummyData(
             CourseRepository courseRepository,
@@ -39,28 +40,10 @@ public class PapApplication {
             UserRepository userRepository,
             ReviewRepository reviewRepository,
             CommentRepository commentRepository,
-            PasswordEncoder passwordEncoder
+            DummyDataGenerator generator
     ) {
         return (args) -> {
-            var user_1 = userRepository.save(new User("rdeckard", "rdeckard@example.com", passwordEncoder.encode("password"), "ROLE_USER", true));
-            var user_2 = userRepository.save(new User("rbatty", "rbatty@example.com", passwordEncoder.encode("password"), "ROLE_USER", true));
-            var admin_1 = userRepository.save(new User("admin", "admin@example.com", passwordEncoder.encode("password"), "ROLE_ADMIN", true));
-
-            var teacher_1 = teacherRepository.save(new Teacher("mgr. Jan Kowalski"));
-            var teacher_2 = teacherRepository.save(new Teacher("mgr. Ann Nowak"));
-
-            var course_1 = courseRepository.save(new Course("Angielski w biznesie", "Angielski", "Biznesowy", "B2+", null, teacher_1));
-            var course_2 = courseRepository.save(new Course("Język angielski poziom C1", "Angielski", "Ogólny", "C1", "M15", teacher_1));
-            var course_3 = courseRepository.save(new Course("Język niemiecki, poziom A2", "Niemiecki", "Akademicki", "A2", "M6", teacher_2));
-            var course_4 = courseRepository.save(new Course("Język włoski dla początkujących", "Włoski", "Akademicki", "A1", "M1", teacher_2));
-
-            var review_1 = reviewRepository.save(new Review(user_1, course_1, "Dobrze prowadzony kurs, wymagający nauczyciel", 8));
-            var review_2 = reviewRepository.save(new Review(user_2, course_1, "Zbyt duże wymagania do studentów", 3));
-            var review_3 = reviewRepository.save(new Review(user_2, course_4, "Świetne wprowadzenie do języka", 10));
-            var review_4 = reviewRepository.save(new Review(user_1, course_3, "W porządku", 6));
-
-            var comment_1 = commentRepository.save(new Comment("Przesada", review_2, user_1));
-            var comment_2 = commentRepository.save(new Comment("Pełna zgoda", review_3, user_1));
+            generator.addDummyData();
 
             var teachers = teacherRepository.findAll();
             log.info("Added teachers:");
---
+++ backend/src/main/java/pl/edu/pw/pap/utils/DummyDataGenerator.java
@@ -0,0 +1,64 @@
+package pl.edu.pw.pap.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.edu.pw.pap.comment.Comment;
+import pl.edu.pw.pap.comment.CommentRepository;
+import pl.edu.pw.pap.course.Course;
+import pl.edu.pw.pap.course.CourseRepository;
+import pl.edu.pw.pap.review.Review;
+import pl.edu.pw.pap.review.ReviewRepository;
+import pl.edu.pw.pap.teacher.Teacher;
+import pl.edu.pw.pap.teacher.TeacherRepository;
+import pl.edu.pw.pap.user.User;
+import pl.edu.pw.pap.user.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class DummyDataGenerator {
+    private final UserRepository userRepository;
+    private final TeacherRepository teacherRepository;
+    private final CourseRepository courseRepository;
+    private final ReviewRepository reviewRepository;
+    private final CommentRepository commentRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User user_1;
+    public User user_2;
+    public User admin_1;
+    public Teacher teacher_1;
+    public Teacher teacher_2;
+    public Course course_1;
+    public Course course_2;
+    public Course course_3;
+    public Course course_4;
+    public Review review_1;
+    public Review review_2;
+    public Review review_3;
+    public Review review_4;
+    public Comment comment_1;
+    public Comment comment_2;
+
+    public void addDummyData() {
+        user_1 = userRepository.save(new User("rdeckard", "rdeckard@example.com", passwordEncoder.encode("password"), "ROLE_USER", true));
+        user_2 = userRepository.save(new User("rbatty", "rbatty@example.com", passwordEncoder.encode("password"), "ROLE_USER", true));
+        admin_1 = userRepository.save(new User("admin", "admin@example.com", passwordEncoder.encode("password"), "ROLE_ADMIN", true));
+
+        teacher_1 = teacherRepository.save(new Teacher("mgr. Jan Kowalski"));
+        teacher_2 = teacherRepository.save(new Teacher("mgr. Ann Nowak"));
+
+        course_1 = courseRepository.save(new Course("Angielski w biznesie", "Angielski", "Biznesowy", "B2+", null, teacher_1));
+        course_2 = courseRepository.save(new Course("Język angielski poziom C1", "Angielski", "Ogólny", "C1", "M15", teacher_1));
+        course_3 = courseRepository.save(new Course("Język niemiecki, poziom A2", "Niemiecki", "Akademicki", "A2", "M6", teacher_2));
+        course_4 = courseRepository.save(new Course("Język włoski dla początkujących", "Włoski", "Akademicki", "A1", "M1", teacher_2));
+
+        review_1 = reviewRepository.save(new Review(user_1, course_1, "Dobrze prowadzony kurs, wymagający nauczyciel", 8));
+        review_2 = reviewRepository.save(new Review(user_2, course_1, "Zbyt duże wymagania do studentów", 3));
+        review_3 = reviewRepository.save(new Review(user_2, course_4, "Świetne wprowadzenie do języka", 10));
+        review_4 = reviewRepository.save(new Review(user_1, course_3, "W porządku", 6));
+
+        comment_1 = commentRepository.save(new Comment("Przesada", review_2, user_1));
+        comment_2 = commentRepository.save(new Comment("Pełna zgoda", review_3, user_1));
+    }
+}

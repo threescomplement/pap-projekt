48ecd517e91dae48f92efb2207e1abe3fc2f42d7
mgarbowski <mikolaj.garbowski@gmail.com>
2023-12-27 16:52:56 +0100

    add integration tests for password reset process

--- backend/src/test/java/pl/edu/pw/pap/user/UserIntegrationTest.java
+++ backend/src/test/java/pl/edu/pw/pap/user/UserIntegrationTest.java
@@ -8,12 +8,21 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.edu.pw.pap.PapApplication;
-import pl.edu.pw.pap.user.emailverification.EmailVerificationTokenRepository;
+import pl.edu.pw.pap.email.EmailSender;
 import pl.edu.pw.pap.user.emailverification.EmailVerificationRequest;
+import pl.edu.pw.pap.user.emailverification.EmailVerificationTokenRepository;
+import pl.edu.pw.pap.user.passwordreset.ResetPasswordRequest;
+import pl.edu.pw.pap.user.passwordreset.ResetPasswordTokenRepository;
+import pl.edu.pw.pap.user.passwordreset.SendResetPasswordEmailRequest;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static pl.edu.pw.pap.utils.UrlBuilder.buildUrl;
 
 /**
@@ -30,12 +39,16 @@ public class UserIntegrationTest {
     private int port;
 
     @MockBean
-    private JavaMailSender fakeEmailSender;  // Do not try to send actual emails
+    private EmailSender fakeEmailSender;  // Do not try to send actual emails
 
     @Autowired
     private UserRepository userRepository;
     @Autowired
     private EmailVerificationTokenRepository tokenRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private ResetPasswordTokenRepository passwordTokenRepository;
 
     TestRestTemplate restTemplate = new TestRestTemplate();
     HttpHeaders headers = new HttpHeaders();
@@ -67,7 +80,7 @@ public class UserIntegrationTest {
         assertNotNull(dbUser);
         assertEquals("new_user", dbUser.getUsername());
         assertEquals("new_user@example.com", dbUser.getEmail());
-        assertNotNull(dbUser.getPassword());  // TODO how to check it properly?
+        assertTrue(passwordEncoder.matches("password", dbUser.getPassword()));
         assertEquals("ROLE_USER", dbUser.getRole());
         assertFalse(dbUser.getEnabled());
     }
@@ -97,5 +110,76 @@ public class UserIntegrationTest {
         assertTrue(verifyResponse.getBody().getEnabled());
         assertTrue(userRepository.findByUsername(registerRequest.username()).get().getEnabled());
 
+    }
+
+    @Test
+    public void resetPasswordSuccess() throws Exception {
+        // Create user
+        var user = new User(
+                "user_1",
+                "user@example.com",
+                passwordEncoder.encode("password"),
+                "ROLE_USER",
+                true
+        );
+        userRepository.save(user);
+
+        // Request email to be sent
+        var sendEmailResponse = restTemplate.exchange(
+                buildUrl("/api/users/send-reset-email", port),
+                HttpMethod.POST,
+                new HttpEntity<>(new SendResetPasswordEmailRequest("user@example.com"), headers),
+                String.class
+        );
+        assertEquals(HttpStatusCode.valueOf(200), sendEmailResponse.getStatusCode());
+
+        // Check if token was generated
+        var token = passwordTokenRepository.findAll()
+                .stream()
+                .filter(t -> t.getEmail().equals(user.getEmail()))
+                .findFirst()
+                .get();
+
+        // Check that email was supposed to be sent with correct parameters
+        verify(fakeEmailSender).sendEmail(eq("user@example.com"), contains(token.getToken()), eq("Reset your password"));
+
+        // Attempt to change the password
+        var resetPasswordResponse = restTemplate.exchange(
+                buildUrl("/api/users/reset-password", port),
+                HttpMethod.POST,
+                new HttpEntity<>(new ResetPasswordRequest("newPassword", token.getToken())),
+                String.class
+        );
+
+        // Check if password was updated
+        assertEquals(HttpStatusCode.valueOf(200), resetPasswordResponse.getStatusCode());
+        var updatedUser = userRepository.findById(user.getId()).get();
+        assertTrue(passwordEncoder.matches("newPassword", updatedUser.getPassword()));
+
+        // Check if token was deleted afterwards
+        assertFalse(passwordTokenRepository.existsById(token.getId()));
+    }
+
+    @Test
+    public void sendResetPasswordEmailWithIncorrectEmail() {
+        var response = restTemplate.exchange(
+                buildUrl("/api/users/send-reset-email", port),
+                HttpMethod.POST,
+                new HttpEntity<>(new SendResetPasswordEmailRequest("iDoNotExist@example.com"), headers),
+                String.class
+        );
+
+        assertEquals(HttpStatusCode.valueOf(400), response.getStatusCode());
+    }
+
+    @Test
+    public void resetPasswordWithInvalidToken() {
+        var response = restTemplate.exchange(
+                buildUrl("/api/users/reset-password", port),
+                HttpMethod.POST,
+                new HttpEntity<>(new ResetPasswordRequest("newPassword", UUID.randomUUID().toString())),
+                String.class
+        );
+        assertEquals(HttpStatusCode.valueOf(400), response.getStatusCode());
     }
 }

240b06afce6d967b47df320cd9071179994d3a11
Michal Luszczek <michluszcz@gmail.com>
2024-01-02 17:41:21 +0100

    created convertReviewRaportToDto without links

--- backend/src/main/java/pl/edu/pw/pap/report/ReportService.java
+++ backend/src/main/java/pl/edu/pw/pap/report/ReportService.java
@@ -3,7 +3,11 @@ package pl.edu.pw.pap.report;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.edu.pw.pap.comment.report.CommentReportRepository;
+import pl.edu.pw.pap.review.report.ReviewReport;
 import pl.edu.pw.pap.review.report.ReviewReportRepository;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 
 @Service
@@ -12,5 +16,17 @@ public class ReportService {
 
     private final ReviewReportRepository reviewReportRepostiory;
     private final CommentReportRepository commentReportRepository;
+
+    public ReportDTO convertReviewRaportToDto(ReviewReport report){
+
+        var reportDTO = ReportDTO.builder()
+                .reportedText(report.getReported().getOpinion())
+                .reportingUsername(report.getReportingUser().getUsername())
+                .reason(report.getReason())
+                .build();
+
+        // TODO: Add links once controller is done
+        return reportDTO.add();
+    }
 
 }

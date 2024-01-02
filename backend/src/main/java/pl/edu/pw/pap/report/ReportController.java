16f085ec53b6fd96edf26afeadfbe281c1e644d6
Michal Luszczek <michluszcz@gmail.com>
2024-01-02 17:42:15 +0100

    added reportService to ReportController, wrote what methods need to be added

--- backend/src/main/java/pl/edu/pw/pap/report/ReportController.java
+++ backend/src/main/java/pl/edu/pw/pap/report/ReportController.java
@@ -2,11 +2,24 @@ package pl.edu.pw.pap.report;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ReportController {
+
+    private final ReportService reportService;
+
+    // oodzielne do dodawania, usuwiania, pobierania pojedynczych (nwm czy potrzebne),
+    // wspólny do pobierania wszystkich
+
+    @GetMapping("/api/admin/reports/comments/{commentReportId}")
+    public ReportDTO getCommentReport(@PathVariable Long commentReportId){
+        return null;
+    }
+
 
 
 

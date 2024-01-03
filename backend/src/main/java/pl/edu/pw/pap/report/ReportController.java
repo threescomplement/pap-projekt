c38317e8caba1db2eaa6f7501758608635e327c5
Michal Luszczek <michluszcz@gmail.com>
2024-01-03 15:26:06 +0100

    ReportController signatures of DELETE, POST and GET methods

--- backend/src/main/java/pl/edu/pw/pap/report/ReportController.java
+++ backend/src/main/java/pl/edu/pw/pap/report/ReportController.java
@@ -2,9 +2,20 @@ package pl.edu.pw.pap.report;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.LinkRelation;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.mediatype.hal.HalModelBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.pw.pap.security.UserPrincipal;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -15,12 +26,50 @@ public class ReportController {
     // oodzielne do dodawania, usuwiania, pobierania pojedynczych (nwm czy potrzebne),
     // wspólny do pobierania wszystkich
 
-    @GetMapping("/api/admin/reports/comments/{commentReportId}")
-    public ReportDTO getCommentReport(@PathVariable Long commentReportId){
+    @PostMapping("api/courses/{courseId}/reviews/{username}/reports")
+    public ReportDTO reportReview(@PathVariable Long courseId,
+                                  @PathVariable String username,
+                                  @RequestBody ReportRequest reportRequest,
+                                  @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        return null;
+    }
+
+    @PostMapping("api/comments/{commentId}/reports")
+    public ReportDTO reportComment(@PathVariable Long commentId,
+                                   @RequestBody ReportRequest reportRequest,
+                                   @AuthenticationPrincipal UserPrincipal userPrincipal) {
         return null;
     }
 
 
+    @GetMapping("api/admin/reports")
+    public RepresentationModel<ReportDTO> getAllReports() {
+        List<ReportDTO> reports = reportService.getAllReports();
+        return HalModelBuilder.emptyHalModel()
+                .embed(reports.isEmpty() ? Collections.emptyList() : reports, LinkRelation.of("reports"))
+                .link(linkTo(methodOn(ReportController.class).getAllReports()).withSelfRel())
+                .build();
+    }
 
+    @GetMapping("/api/admin/reports/comments/{commentReportId}")
+    public ReportDTO getCommentReport(@PathVariable Long commentReportId) {
+        return null;
+    }
+
+    @GetMapping("/api/admin/reports/reviews/{reviewReportId}")
+    public ReportDTO getReviewReport(@PathVariable Long reviewReportId) {
+        return null;
+    }
+
+
+    @DeleteMapping("/api/admin/reports/comments/{commentReportId}")
+    public ResponseEntity<ReportDTO> deleteCommentReport(@PathVariable Long commentReportId) {
+        return null;
+    }
+
+    @DeleteMapping("/api/admin/reports/reviews/{reviewReportId}")
+    public ResponseEntity<ReportDTO> deleteReviewReport(@PathVariable Long reviewReportId) {
+        return null;
+    }
 
 }

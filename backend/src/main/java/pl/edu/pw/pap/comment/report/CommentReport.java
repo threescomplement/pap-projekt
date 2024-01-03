3f8e237737cdd64daee2a24131e0f70d5d0ff73c
Michal Luszczek <michluszcz@gmail.com>
2024-01-03 16:50:48 +0100

    remove unused imports

--- backend/src/main/java/pl/edu/pw/pap/comment/report/CommentReport.java
+++ backend/src/main/java/pl/edu/pw/pap/comment/report/CommentReport.java
@@ -1,8 +1,6 @@
 package pl.edu.pw.pap.comment.report;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import pl.edu.pw.pap.comment.Comment;

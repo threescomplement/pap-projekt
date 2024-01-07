e2b6d903cc92e20c08b447374fa9b4266e9a479c
Michal Luszczek <michluszcz@gmail.com>
2024-01-07 18:49:01 +0100

    overwritten findById in ReviewReportRepository

--- backend/src/main/java/pl/edu/pw/pap/review/report/ReviewReportRepository.java
+++ backend/src/main/java/pl/edu/pw/pap/review/report/ReviewReportRepository.java
@@ -3,6 +3,10 @@ package pl.edu.pw.pap.review.report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReviewReportRepository extends JpaRepository<ReviewReport, Long> {
+
+    public Optional<ReviewReport> findById(Long reportId);
 }

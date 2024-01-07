db391db16843cd11ac965d6b394a1d951274776a
Michal Luszczek <michluszcz@gmail.com>
2024-01-07 14:42:14 +0100

    added error margin to Double equality assertion

--- backend/src/test/java/pl/edu/pw/pap/teacher/TeacherIntegrationTests.java
+++ backend/src/test/java/pl/edu/pw/pap/teacher/TeacherIntegrationTests.java
@@ -69,9 +69,9 @@ public class TeacherIntegrationTests {
         var json = JsonPath.parse(response.getBody());
         assertEquals(1, (int) json.read("$.id"));
         assertEquals(data.teacher_1.getName(), json.read("$.name"));
-        assertEquals(5.5, (double) (json.read("$.averageEaseRating")));
-        assertEquals(5.5, (double) (json.read("$.averageInterestRating")));
-        assertEquals(5.5, (double) (json.read("$.averageEngagementRating")));
+        assertEquals(5.5, (double) (json.read("$.averageEaseRating")), 0.01);
+        assertEquals(5.5, (double) (json.read("$.averageInterestRating")), 0.01);
+        assertEquals(5.5, (double) (json.read("$.averageEngagementRating")), 0.01);
         // TODO        assertEquals(2, (int) json.read("$.numRatings"));
         assertTrue(json.read("$._links.self.href").toString().endsWith("/api/teachers/1"));
         assertTrue(json.read("$._links.courses.href").toString().contains("/api/courses?name=&language=all&module=all&type=all&level=all&teacherName=mgr.%20Jan%20Kowalski"));

ad19116a10da1137a77a33e8d87ef80b7506077a
maksym <01178511@pw.edu.pl>
2024-01-08 16:54:09 +0100

    quick fix to make all buttons nice and blue as the ones defined in singlereview.css, to fix when styling

--- frontend/src/pages/review/SingleReview.tsx
+++ frontend/src/pages/review/SingleReview.tsx
@@ -8,6 +8,7 @@ import {User} from "../../lib/User";
 import useUser from "../../hooks/useUser";
 import {EditBar} from "../../components/EditBar";
 import ErrorBox from "../../components/ErrorBox";
+import "./SingleReview.css"
 
 export function SingleReview() {
     const {courseId, authorUsername} = useParams();

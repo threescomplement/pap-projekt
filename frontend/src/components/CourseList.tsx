444fcab113bccb5e221919b667edb717e8d0c36a
maksym <01178511@pw.edu.pl>
2024-01-11 22:43:23 +0100

    add teacher link to courserow

--- frontend/src/components/CourseList.tsx
+++ frontend/src/components/CourseList.tsx
@@ -1,7 +1,8 @@
 import {Course} from "../lib/Course";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import {ratingToPercentage} from "../lib/utils";
+import {Teacher, TeacherService} from "../lib/Teacher";
 
 interface CourseListProps {
     courses: Course[]
@@ -9,10 +10,11 @@ interface CourseListProps {
 
 export default function CourseList({courses}: CourseListProps) {
 
-    return courses.length !== 0 ?<table>
+    return courses.length !== 0 ? <table>
         <tbody>
         <tr id="headers">
             <th>Nazwa</th>
+            <th>Lektor</th>
             <th>Jak łatwy?</th>
             <th>Jak interesujący?</th>
             <th>Jak angażujący?</th>
@@ -31,9 +33,17 @@ interface CourseProps {
 
 export function CourseRow({course}: CourseProps) {
     const hasRatings = parseInt(course.numberOfRatings) !== 0;
+    const [teacherName, setTeacherName] = useState<null | string>(null);
+    useEffect(() => {
+        TeacherService.fetchTeacher(course.teacherId)
+            .then(t=>setTeacherName(t.name))
+    }, []);
+
+
     return (
         <tr id={course.id}>
             <td><Link to={`/courses/${course.id}`}> {course.name}</Link></td>
+            {teacherName != null && <td><Link to={`/teachers/${course.teacherId}`}>{teacherName}</Link></td>}
             <td> {hasRatings ? ratingToPercentage(course.averageEaseRating) : "N/A"}</td>
             <td> {hasRatings ? ratingToPercentage(course.averageInterestRating) : "N/A"}</td>
             <td>{hasRatings ? ratingToPercentage(course.averageEngagementRating) : "N/A"}</td>

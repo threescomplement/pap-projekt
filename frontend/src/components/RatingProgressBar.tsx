7c969c83782789c7717fd8a8814e6034488e9aad
maksym <01178511@pw.edu.pl>
2024-01-18 13:23:01 +0100

    dynamic progress bar color

--- frontend/src/components/RatingProgressBar.tsx
+++ frontend/src/components/RatingProgressBar.tsx
@@ -6,13 +6,22 @@ interface RatingProgressBarProps {
 }
 export default function RatingProgressBar({value}: RatingProgressBarProps) {
     const parsedValue = parseFloat(value)
+    const progresBarColor = parsedValue <=4
+        ? '#ff6166'
+        : (parsedValue <= 7.5 ? '#fdfd96': '#c1e1c1');
+
+    const dynamicProgressBarStyles = {
+        'background-color': progresBarColor,
+        transform: `translateX(-${100 - 10*parsedValue}%)`
+    };
+
     return <div>
         <Progress.Root className={styles.ProgressRoot}
         max={10}
         value={parsedValue}
         >
             <Progress.Indicator className={styles.ProgressIndicator}
-                                style={{ transform: `translateX(-${100 - 10*parsedValue}%)` }}
+                                style={dynamicProgressBarStyles}
             />
         </Progress.Root>
     </div>

df45ebf6ebef57e25940c562b57f80aa37b1a376
Michal Luszczek <michluszcz@gmail.com>
2024-01-18 20:21:39 +0100

    Added methods in report repositories for finding by resolved status

--- backend/src/main/java/pl/edu/pw/pap/review/report/ReviewReportRepository.java
+++ backend/src/main/java/pl/edu/pw/pap/review/report/ReviewReportRepository.java
@@ -3,10 +3,12 @@ package pl.edu.pw.pap.review.report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ReviewReportRepository extends JpaRepository<ReviewReport, Long> {
     Long removeByCourseIdAndReviewerUsername(Long courseId, String reviewerUsername);
 
+    List<ReviewReport> findByResolved(Boolean resolvedStatus);
 }
